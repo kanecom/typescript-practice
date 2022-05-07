9fe2329d9a09329ef68cebe320b7e2908efd2936
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-08 00:07:39 +0900

    feat: FizzBuzzの結果を1行で出力するコードを追加

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,3 +1,4 @@
+// 通常のFizzBuzz
 for (let i: number = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
@@ -9,3 +10,16 @@ for (let i: number = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+// 1行で出力するFizzBuzz
+const array: string[] = [];
+
+for (let i: number = 1; i <= 100; i++) {
+    if (i % 3 !== 0 && i % 5 !== 0) {
+        array.push(String(i));
+    } else {
+        array.push(`${i % 3 === 0 ? "Fizz" : ""}${i % 5 === 0 ? "Buzz" : ""}`);
+    }
+}
+
+console.log(array.join(" "));
