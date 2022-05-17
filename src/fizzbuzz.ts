8a4f72a3fa5c9d76b6c96330e005cf4580b3a530
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-17 21:55:49 +0900

    refactor: for文を4.6.1節に従いfor-of文に変更

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -9,8 +9,13 @@ const getFizzBuzzString = (number: number): string => {
     return String(number);
 };
 
+const sequence = (start: number, end: number): number[] => {
+    const length = end - start + 1;
+    return [...Array(length)].map((_, i) => i + start);
+};
+
 // 通常のFizzBuzz
-for (let i: number = 1; i <= 100; i++) {
+for (const i of sequence(1, 100)) {
     const message = getFizzBuzzString(i);
     console.log(message);
 }
@@ -18,7 +23,7 @@ for (let i: number = 1; i <= 100; i++) {
 // 1行で出力するFizzBuzz
 const array: string[] = [];
 
-for (let i: number = 1; i <= 100; i++) {
+for (const i of sequence(1, 100)) {
     if (i % 3 !== 0 && i % 5 !== 0) {
         array.push(String(i));
     } else {
