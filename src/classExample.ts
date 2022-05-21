dca4f501573ec5b8465c12ad5ee0c5a5adba3313
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-21 23:21:06 +0900

    refactor: 5.6.1節のコードをクラスを使って書き直した

--- src/classExample.ts
+++ src/classExample.ts
@@ -1,21 +1,19 @@
-export type User = {
-    name: string;
-    age: number;
-}
+export class User {
+    #name: string;
+    #age: number;
 
-function createUser(name: string, age: number): User {
-    if (name === "") {
-        throw new Error("名前は空にできません");
+    constructor(name: string, age: number) {
+        if (name === "") {
+            throw new Error("名前は空にできません");
+        }
+        this.#name = name;
+        this.#age = age;
     }
-    return {
-        name,
-        age,
-    };
+
+    public getMessage(message: string): string {
+        return `${this.#name} (${this.#age}) 「${message}」`;
+    }
 }
 
-function getMessage(user: User, message: string): string {
-    return `${user.name} (${user.age}) 「${message}」`;
-}
-
-const uhyo = createUser("uhyo", 26);
-console.log(getMessage(uhyo, "こんにちは"));
+const uhyo = new User("uhyo", 26);
+console.log(uhyo.getMessage("こんにちは"));
