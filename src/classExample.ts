0c22d4be88da52c868f205785f21673bf32b2f4c
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-21 23:13:07 +0900

    feat: 5.6.1節のお題コードを追加

---
+++ src/classExample.ts
@@ -0,0 +1,21 @@
+export type User = {
+    name: string;
+    age: number;
+}
+
+function createUser(name: string, age: number): User {
+    if (name === "") {
+        throw new Error("名前は空にできません");
+    }
+    return {
+        name,
+        age,
+    };
+}
+
+function getMessage(user: User, message: string): string {
+    return `${user.name} (${user.age}) 「${message}」`;
+}
+
+const uhyo = createUser("uhyo", 26);
+console.log(getMessage(uhyo, "こんにちは"));
