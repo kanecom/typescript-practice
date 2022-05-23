54a9c28c397405b4f8eb0104eed9ff9977b619d0
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-23 20:35:55 +0900

    feat: 6.1.2節のコードを追加・更新

--- src/advancedTypeExample.ts
+++ src/advancedTypeExample.ts
@@ -3,20 +3,24 @@ export const chapterName = "第6章 高度な型";
 // ユニオン型
 type Animal = {
     species: string;
+    age: string;
 };
 
 type Human = {
     name: string;
+    age: number;
 };
 
 type User = Animal | Human;
 
 const tama: User = {
-    species: "Felis silvestris catus"
+    species: "Felis silvestris catus",
+    age: "永遠の17歳"
 };
 
 const uhyo: User = {
-    name: "uhyo"
+    name: "uhyo",
+    age: 26
 };
 
 // ERROR
@@ -28,3 +32,17 @@ const uhyo: User = {
 // function getName(user: User): string {
 //     return user.name;
 // }
+
+function showAge(user: User) {
+    const age = user.age; // string | number 型
+    console.log(age);
+}
+
+type MysteryFunc =
+    | ((str: string) => string)
+    | ((str: string) => number);
+
+function useFunc(func: MysteryFunc) {
+    const result = func("uhyo"); // string | number 型
+    console.log(result);
+}
