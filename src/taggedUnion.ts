8501ce359708070a58bf175123d64d46b0d8ed0a
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-30 21:10:46 +0900

    feat: 6.8.5節のコードを一旦自力で実装

--- src/taggedUnion.ts
+++ src/taggedUnion.ts
@@ -17,3 +17,22 @@ const printNumber = (input: Option<number>) => {
 };
 
 const isSome = <T>(input: Option<T>): input is Some<T> => { return input.tag === "some" };
+
+const mapOption = <T1, T2> (input: Option < T1 >, func: (value:T1) => T2) => {
+    if (isSome(input)) {
+        const value = func(input.value);
+        return {
+            ...input,
+            value
+        }
+    }
+    return input;
+};
+
+const doubleOperation = (obj: Option<number>) => mapOption(obj, x => x * 2);
+
+const four: Option<number> = { tag: "some", value: 4 };
+const nothing: Option<number> = { tag: "none" };
+
+console.log(doubleOperation(four));
+console.log(doubleOperation(nothing));
