10edb104ccdc09a43edf22bb1aeed8707501a755
kanecom <66767522+kanecom@users.noreply.github.com>
2022-05-30 19:29:08 +0900

    feat: 6.4.2節のコードを追加

--- src/advancedTypeExample.ts
+++ src/advancedTypeExample.ts
@@ -199,3 +199,27 @@ const jiro: Person = {
 
 const jiro2 = setAge(jiro, 27);
 console.log(jiro2);
+
+// keyof型
+type HumanKeys = keyof Human;
+let key: HumanKeys = "name";
+key = "age";
+// ERROR
+// key = "hoge";
+
+const mmConversionTable = {
+    mm: 1,
+    m: 1e3,
+    km: 1e6
+};
+
+function convertUnits(value: number, unit: keyof typeof mmConversionTable) {
+    const mmValue = value * mmConversionTable[unit];
+    return {
+        mm: mmValue,
+        m: mmValue / 1e3,
+        km: mmValue / 1e6
+    };
+}
+
+console.log(convertUnits(5600, "m"));
