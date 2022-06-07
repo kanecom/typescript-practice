ee0ee0be17728170af61631867c638d9cd1ed4f8
kanecom <66767522+kanecom@users.noreply.github.com>
2022-06-07 22:22:57 +0900

    feat: 8.3.1節のコードを追加

--- src/asynchronousProcessing.ts
+++ src/asynchronousProcessing.ts
@@ -5,3 +5,12 @@ setTimeout(() => {
     console.log("タイマーが呼び出されました");
 }, 3000);
 console.log("タイマーを設定しました");
+
+// Promise
+import { readFile } from "fs/promises";
+
+const p = readFile("uhyo.txt", "utf8");
+
+p.then((data) => {
+    console.log(data)
+});
